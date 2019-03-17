b52878cc7cac109b72cbf3a44920416d38e799f0
ornokrean <or.nokrean@mail.huji.ac.il>
2019-03-17 20:18:29 +0200

    sd

--- osm.cpp
+++ osm.cpp
@@ -1,20 +1,12 @@
-//
-// Created by Erez on 17-Mar-19.
-//
-
 #include "osm.h"
 #include <sys/time.h>
 
-int osm_init(){
+int osm_init(){}
 
-}
-int osm_finalizer(){
-
-
-}
+int osm_finalizer(){}
 
 double osm_operation_time(unsigned int iterations){
-    struct timeval start;
+    struct timeval start, end;
     gettimeofday(&start, NULL);
     if (iterations==0){
         iterations=1000;
@@ -22,11 +14,9 @@ double osm_operation_time(unsigned int iterations){
     for (int i=0; i < iterations; i++){
         1+1;
     }
-    struct timeval  end;
-
     gettimeofday(&end, NULL);
-    return (end.tv_sec * 1000000 + end.tv_usec)
-                            - (start.tv_sec * 1000000 + start.tv_usec);
+    return double((end.tv_sec * 1000000000 + end.tv_usec * 1000)
+                            - (start.tv_sec * 1000000000 + start.tv_usec * 1000))/iterations;
 }
 
 void func(){}
