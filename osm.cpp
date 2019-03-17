fbf84c3fd3640bc32ff8fbf75b886da98619e898
ornokrean <or.nokrean@mail.huji.ac.il>
2019-03-17 19:43:32 +0200

    sd

--- osm.cpp
+++ osm.cpp
@@ -19,15 +19,26 @@ double osm_operation_time(unsigned int iterations){
     }
 }
 
+void func(){
+    return;
+}
 double osm_function_time(unsigned int iterations){
     if (iterations==0){
         iterations=1000;
+    }
+    for (int i = 0; i < iterations; ++i)
+    {
+        func();
     }
 }
 
 double osm_syscall_time(unsigned int iterations){
     if (iterations==0){
         iterations=1000;
+    }
+    for (int i = 0; i < iterations; ++i)
+    {
+        OSM_NULLSYSCALL;
     }
 
 
