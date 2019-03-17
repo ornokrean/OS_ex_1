ae72b1f262e37e6f9e0e5e4fe49a53d4123f2159
ornokrean <or.nokrean@mail.huji.ac.il>
2019-03-17 20:23:20 +0200

    sd

--- osm.cpp
+++ osm.cpp
@@ -1,30 +1,39 @@
 #include "osm.h"
 #include <sys/time.h>
 
-double clac_total_time(unsigned int iterations, const timeval &start, const timeval &end);
+double calc_total_time(unsigned int iterations, const timeval &start, const timeval &end);
 
-int osm_init(){}
+int osm_init()
+{}
 
-int osm_finalizer(){}
+int osm_finalizer()
+{}
 
-double osm_operation_time(unsigned int iterations){
+double osm_operation_time(unsigned int iterations)
+{
     struct timeval start, end;
     gettimeofday(&start, NULL);
-    if (iterations==0){
-        iterations=1000;
+    if (iterations == 0)
+    {
+        iterations = 1000;
     }
-    for (int i=0; i < iterations; i++){
-        1+1;
+    for (int i = 0; i < iterations; i++)
+    {
+        1 + 1;
     }
     gettimeofday(&end, NULL);
-    return clac_total_time(iterations, start, end);
+    return calc_total_time(iterations, start, end);
 }
 
 
-void func(){}
-double osm_function_time(unsigned int iterations){
-    if (iterations==0){
-        iterations=1000;
+void func()
+{}
+
+double osm_function_time(unsigned int iterations)
+{
+    if (iterations == 0)
+    {
+        iterations = 1000;
     }
     struct timeval start, end;
     gettimeofday(&start, NULL);
@@ -33,13 +42,15 @@ double osm_function_time(unsigned int iterations){
         func();
     }
     gettimeofday(&end, NULL);
-    return clac_total_time(iterations, start, end);
+    return calc_total_time(iterations, start, end);
 
 }
 
-double osm_syscall_time(unsigned int iterations){
-    if (iterations==0){
-        iterations=1000;
+double osm_syscall_time(unsigned int iterations)
+{
+    if (iterations == 0)
+    {
+        iterations = 1000;
     }
     struct timeval start, end;
     gettimeofday(&start, NULL);
@@ -48,10 +59,10 @@ double osm_syscall_time(unsigned int iterations){
         OSM_NULLSYSCALL;
     }
     gettimeofday(&end, NULL);
-    return clac_total_time(iterations, start, end);
+    return calc_total_time(iterations, start, end);
 }
 
-double clac_total_time(unsigned int iterations, const timeval &start, const timeval &end)
+double calc_total_time(unsigned int iterations, const timeval &start, const timeval &end)
 {
     return double((end.tv_sec * 1000000000 + end.tv_usec * 1000)
                   - (start.tv_sec * 1000000000 + start.tv_usec * 1000)) / iterations;
