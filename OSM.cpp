a11414ae489601f84e2bd20e3f8a8d94027d5851
erezuni <erez.hecht@mail.huji.ac.il>
2019-03-17 19:30:23 +0200

    init

---
+++ OSM.cpp
@@ -0,0 +1,19 @@
+//
+// Created by Erez on 17-Mar-19.
+//
+
+#include "OSM.h"
+
+int osm_init(){
+}
+int osm_finalizer(){}
+
+double osm_operation_time(unsigned int iterations){
+    for (int i=0; i < iterations; i++){
+        1+1;
+    }
+}
+double osm_function_time(unsigned int iterations){}
+
+double osm_syscall_time(unsigned int iterations){}
+
